a5c64b826e8bcd4be76c853ef597496fc1415fac
Memory <1448208262@qq.com>
2024-06-18 23:17:06 +0800

    临时性提交

--- src/main/java/cn/zhiyou/action/TestAction.java
+++ src/main/java/cn/zhiyou/action/TestAction.java
@@ -1,9 +1,27 @@
 package cn.zhiyou.action;
 
+import cn.hutool.db.ds.simple.SimpleDataSource;
+import cn.zhiyou.config.DataBaseSetting;
+import cn.zhiyou.entity.PackageAndPath;
+import cn.zhiyou.notify.OpenDataBaseSettingNotificationAction;
+import cn.zhiyou.ui.CreateMyBatisMapperWindow;
+import cn.zhiyou.utils.ActionUtil;
+import cn.zhiyou.utils.NotificationUtil;
+import com.intellij.notification.NotificationAction;
+import com.intellij.notification.NotificationType;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.util.Collection;
 
 /**
  * @author Memory
@@ -15,44 +33,51 @@ public class TestAction extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
+        Project project = event.getProject();
+        // 当前 module
+        Module module = event.getData(PlatformDataKeys.MODULE);
+        if (project == null || module == null) {
+            return;
+        }
 
-        RuntimeException exception = new RuntimeException("Acs");
+        DataBaseSetting dataBaseSetting = DataBaseSetting.getInstance();
+        if (!DataBaseSetting.checkPluginDataSource()) {
+            NotificationUtil.notifyWithLink(
+                    "逆向生成",
+                    "缺失数据源配置",
+                    new NotificationAction[]{new OpenDataBaseSettingNotificationAction()},
+                    NotificationType.WARNING,
+                    project);
+            return;
+        }
 
-        LOG.error(exception.getMessage(), exception);
+        SimpleDataSource simpleDataSource = new SimpleDataSource(
+                dataBaseSetting.url,
+                dataBaseSetting.user,
+                dataBaseSetting.pass,
+                dataBaseSetting.driver);
 
-        // Project project = event.getProject();
-        // ProgressManager.getInstance().run(new Task.Backgroundable(project, "File generation") {
-        //     @Override
-        //     public void run(@NotNull ProgressIndicator progressIndicator) {
-        //         progressIndicator.setIndeterminate(false);
-        //         progressIndicator.setText("File generation in progress...");
-        //         progressIndicator.setFraction(0);
-        //
-        //         try {
-        //             Thread.sleep(500L);
-        //         } catch (InterruptedException e) {
-        //             throw new RuntimeException(e);
-        //         }
-        //
-        //
-        //         ActionUtil.runWriteCommandAction(project, () -> {
-        //
-        //             try {
-        //                 throw new RuntimeException();
-        //             } catch (RuntimeException e) {
-        //                 // LOG.error(e.getMessage(), e);
-        //                 throw e;
-        //             }
-        //
-        //             // ErrorReportSubmitter
-        //
-        //         });
-        //
-        //
-        //         progressIndicator.setFraction(1.0);
-        //         progressIndicator.setText("Finished");
-        //     }
-        // });
+        try (Connection connection = simpleDataSource.getConnection();) {
+            // connection.prepareStatement()
+            DatabaseMetaData metaData = connection.getMetaData();
+
+            ResultSet schemas = metaData.getSchemas();
+
+            while (schemas.next()) {
+                String schemaName = schemas.getString("TABLE_SCHEM");
+                System.out.println("Schema Name: " + schemaName);
+            }
+
+        } catch (Throwable throwable) {
+
+        }
+
+        // 查找所有xml文件
+        Collection<VirtualFile> virtualFiles = ActionUtil.findFilesByExt(project, "xml", module.getModuleScope());
+        // 根据xml文件获取
+        PackageAndPath packageAndPath = CreateMyBatisMapperAction.searchFilePath(project, virtualFiles);
+        // 弹窗
+        new CreateMyBatisMapperWindow(event, project, module, packageAndPath, simpleDataSource, null, null).show();
     }
 
 }
